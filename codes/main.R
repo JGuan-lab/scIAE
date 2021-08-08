8798ff5adee105f707f7158e77a8184b1d1cad94
yinsaiyo <48746135+yinsaiyo@users.noreply.github.com>
2021-08-08 12:13:35 +0800

    Update main.R

--- codes/main.R
+++ codes/main.R
@@ -1,16 +1,24 @@
 source("scIAE.R")
 source("evaluate.R")
 source('get_intersection.R')
+
+######   Prepare data   ######
 train_data <- read.csv("XXXX.csv")
 train_info <- read.csv("XXXX.csv")
 test_data <- read.csv("XXXX.csv")
 test_info <- read.csv("XXXX.csv")
-data_intersection <-get_intersection(train_data,test_data)
-train_data <- data_intersection[[1]]
-test_data <- data_intersection[[2]]
+
+######   Get intersection genes (Optional)   ######
+#data_intersection <-get_intersection(train_data,test_data)
+#train_data <- data_intersection[[1]]
+#test_data <- data_intersection[[2]]
+
+######   Run scIAE   ######
 pred_labels <- scIAE (train_data,
                       train_info,
                       test_data) 
+
+######   Evaluate   ######
 true_labels <- test_info
 result <- evaluate(true_labels,pred_labels)
 print(result)
