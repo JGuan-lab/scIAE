01022e48913aebab6e6647744f4c79cedcf279a3
JGuan-lab <64457671+JGuan-lab@users.noreply.github.com>
2021-08-08 11:32:36 +0800

    main function

--- codes/main.R
+++ codes/main.R
@@ -4,6 +4,9 @@ train_data <- read.csv("XXXX.csv")
 train_info <- read.csv("XXXX.csv")
 test_data <- read.csv("XXXX.csv")
 test_info <- read.csv("XXXX.csv")
+data_intersection <-get_intersection(train_data,test_data)
+train_data <- data_intersection[[1]]
+test_data <- data_intersection[[2]]
 pred_labels <- scIAE (train_data,
                       train_info,
                       test_data) 
